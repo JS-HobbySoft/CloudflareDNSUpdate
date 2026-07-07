3896f8c55ea1c2df5ce2262fd3d06c0549459844
JS-HobbySoft <jshobbysoft@gmail.com>
2026-07-07 18:34:49 -0500

    Remove unused import

--- app/src/main/java/org/jshobbysoft/cloudflarednsupdate/MainActivity.kt
+++ app/src/main/java/org/jshobbysoft/cloudflarednsupdate/MainActivity.kt
@@ -21,7 +21,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+//import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
